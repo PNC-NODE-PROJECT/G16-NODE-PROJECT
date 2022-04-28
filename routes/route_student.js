b19b6cfe60f989d03de04bf4aea2555dfba4f08b
Chansok.OEUN <Chansok.OEUN@pnc.lan>
2022-04-28 07:38:22 +0700

    #12 display question back-end

---
+++ routes/route_student.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router= express.Router();
+
+const data_models = require('../models/models_student.js');
+
+// get data
+router.get('/',(req,res) =>{
+    let data= data_models.get_all_data();
+    res.send(data);
+});
+
+
+// add data
+router.post('/',(req,res)=>{
+    let question = req.body.question;
+    let answer_a = req.body.answer_a;
+    let answer_b = req.body.answer_b;
+    let answer_c = req.body.answer_c;
+    let answer_d = req.body.answer_d;
+    let data = data_models.add_questions(question,answer_a,answer_b,answer_c,answer_d);
+    res.status(200).send({"MESSAGE":"SUCCESSFULL !"})
+})
+
+
+
+
+
+
+//export 
+// console.log(dataModels.load_data());
+module.exports =router;

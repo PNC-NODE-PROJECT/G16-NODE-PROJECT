8b379b929a309395546d5137a37f1921b48c5460
Somnak-Kalan <kalansomnak12@gmail.com>
2022-04-28 12:46:26 +0700

    #2 GET question from back-end to display on DOM

--- models/models_question.js
+++ models/models_question.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const {v4: uuidv4}=require('uuid');
 
-const PATH= './data/data_question.json';
+const PATH= './data/data_student.json';
 
 let load_data = ()=>{
     return JSON.parse(fs.readFileSync(PATH));
 
 };
-// write data
 let save_data = (data)=>{
     fs.writeFileSync(PATH, JSON.stringify(data,null,4));
 }
@@ -15,10 +14,20 @@ let save_data = (data)=>{
 let get_all_data = ()=>{
     return load_data();
 }
-
+//add questions 
+let add_questions = (questions,answer_a,answer_b,answer_c,answer_d)=>{
+    //load_data();
+    let get_load_data = load_data();
+    //store in object
+    let new_data = {"id":uuidv4(),"questions":questions,"answer_a":answer_a,"answer_b":answer_b,"answer_c":answer_c,"answer_d":answer_d};
+    get_load_data.push(new_data);
+    //save data
+    save_data(get_load_data);
+}
 
 
 //EXPORT
+module.exports.load_data =load_data;
+module.exports.add_questions =add_questions;
 module.exports.get_all_data = get_all_data;
-// module.exports.remove_question = remove_question;
 
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express =require('express');
+var cors = require('cors');
 const app = express();
+app.use(cors()); // To allow any origin
+
 const PORT = process.env.PORT || 5000;
-app.use(express.urlencoded({ extended:true}));
+app.use(express.urlencoded({ extended:true})); 
 app.use(express.json());
 
+
+// Define static route
+app.use(express.static("public"));
 // router
 const dataRouter = require('./routes/route_question.js');
 
